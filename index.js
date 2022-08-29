8dd4efe0407911f7a839477216a3cfb29ec515df
Sandra <sandranyambura62@gmail.com>
2022-08-29 22:43:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+
+  return word.split("").reverse().join("");
 }
+  
+  function isPalindrome(word) {
+    const reversedWord = reverse(word);
+  
+    return word === reversedWord;
+  }
+
 
 /* 
   Add your pseudocode here
